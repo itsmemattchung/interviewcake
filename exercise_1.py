6f2b5b97fca981297049c611c0f23b8cf6be5dd2
Matt Chung <matt.chung@fox.com>
2015-04-16 15:13:20 -0700

    Added assertion to test descending stock prices

--- exercise_1.py
+++ exercise_1.py
@@ -24,7 +24,13 @@ class AppleStocks(unittest.TestCase):
         self.stock_prices.append(100)
         self.stock_prices.append(500)
         self.stock_prices.append(400)
+        self.descending_stock_prices = []
+        self.descending_stock_prices.append(500)
+        self.descending_stock_prices.append(400)
+        self.descending_stock_prices.append(100)
+        self.descending_stock_prices.append(50)
 
     def test_get_max_profit(self):
         self.assertEquals(get_max_profit(self.stock_prices)[400], (2,3) )
+        self.assertEquals(get_max_profit(self.descending_stock_prices)[-100], (0,1))
 
