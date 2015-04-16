bbc067c80e9baf3674937864c4279ac0e1336acb
Matt Chung <matt.chung@fox.com>
2015-04-16 15:02:51 -0700

    Complete solution and unit test for apple stock prices

--- exercise_1.py
+++ exercise_1.py
@@ -5,6 +5,16 @@
 import unittest
 
 
+# results[profit] = (buy_index, sell_index)
+def get_max_profit(stock_prices):
+    profits = {}
+    for buy_index, purchase_price in enumerate(stock_prices):
+        future_stocks = stock_prices[buy_index:]
+        for sell_index, sell_price in enumerate(future_stocks, start=buy_index):
+            profits[sell_price - purchase_price] = (buy_index, sell_index)
+    return profits
+
+
 class AppleStocks(unittest.TestCase):
     def setUp(self):
         self.stock_prices = []
@@ -15,5 +25,5 @@ class AppleStocks(unittest.TestCase):
         self.stock_prices.append(400)
 
     def test_get_max_profit(self):
-        self.assertEquals(get_max_profit(self.stock_prices), 400)
+        self.assertEquals(get_max_profit(self.stock_prices)[400], (2,3) )
 
