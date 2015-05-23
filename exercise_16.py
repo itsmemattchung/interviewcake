484d1a0d926fe4a677fb509f87b16325c8ecca0f
matthewchung <itsmemattchung@gmail.com>
2015-05-22 22:41:20 -0700

    Functional max_duffel_bag_value() needs to be refactored

--- exercise_16.py
+++ exercise_16.py
@@ -13,6 +13,25 @@ class CakeThief:
 	# need to work on the below for muscle memory
 	return sorted(cakes,key=lambda cake: self.get_max_cake_value(cake,capacity), reverse=True)
 
+    def get_max_number_of_bags(self, cake, capacity):
+        cake_weight = cake[0]
+        cake_value = cake[1]
+        max_number_of_bags = capacity / cake_weight
+        return max_number_of_bags
+
+    def max_duffel_bag_value(self, cakes, capacity):
+	sorted_cakes = self.sort_cakes(cakes, capacity)
+	current_capacity = capacity
+	max_duffel_bag_value = 0
+	for cake in sorted_cakes:
+	    cake_weight = cake[0]
+	    cake_value = cake[1]
+	    max_bags = self.get_max_number_of_bags(cake, current_capacity)
+	    if max_bags < current_capacity:
+                max_duffel_bag_value += self.get_max_cake_value(cake, current_capacity)
+		current_capacity = current_capacity - ( max_bags * cake_weight)
+	return max_duffel_bag_value 
+
 class TestCakeThief(unittest.TestCase):
     def setUp(self):
 	self.cake_thief = CakeThief()
@@ -28,3 +47,6 @@ class TestCakeThief(unittest.TestCase):
     def test_sort_cakes(self):
 	sorted_cakes = [(3,90), (7,160), (2,15)]
 	self.assertEquals(self.cake_thief.sort_cakes(self.cakes, self.capacity), sorted_cakes)
+
+    def test_max_duffel_bag_value(self):
+	self.assertEquals(555, self.cake_thief.max_duffel_bag_value(self.cakes, self.capacity))
