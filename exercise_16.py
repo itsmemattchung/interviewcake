9d263b8667140ebab0aa73f499cd68439495d0dd
matthewchung <itsmemattchung@gmail.com>
2015-05-22 17:34:00 -0700

    Second test case created for exercise16

--- exercise_16.py
+++ exercise_16.py
@@ -9,14 +9,18 @@ class CakeThief:
       max_number_of_bags = capacity / cake_weight
       return max_number_of_bags * cake_value
 
-
 class TestCakeThief(unittest.TestCase):
     def setUp(self):
-	self.cake_theif = CakeThief()
+	self.cake_thief = CakeThief()
 	self.cakes = [(7,160), (3,90), (2,15)]
 	self.capacity = 20
 
     def test_get_max_cake_value(self):
-	self.assertEquals(self.cake_theif.get_max_cake_value(self.cakes[0], self.capacity), 320)
-	self.assertEquals(self.cake_theif.get_max_cake_value(self.cakes[1], self.capacity), 540)
-	self.assertEquals(self.cake_theif.get_max_cake_value(self.cakes[2], self.capacity), 150)
+	self.assertEquals(self.cake_thief.get_max_cake_value(self.cakes[0], self.capacity), 320)
+	self.assertEquals(self.cake_thief.get_max_cake_value(self.cakes[1], self.capacity), 540)
+	self.assertEquals(self.cake_thief.get_max_cake_value(self.cakes[2], self.capacity), 150)
+
+
+    def test_sort_cakes(self):
+	sorted_cakes = [(3,90), (7,160), (2,15)]
+	self.assertEquals(self.cake_thief.sort_cakes(self.cakes), sorted_cakes)
