4951f116b4d812ede5f7ab241bbe22cecbe62382
matthewchung <itsmemattchung@gmail.com>
2015-05-22 22:19:42 -0700

    Added sort_cakes for exercise 16

--- exercise_16.py
+++ exercise_16.py
@@ -4,10 +4,14 @@ import unittest
 class CakeThief:
 
     def get_max_cake_value(self,cake, capacity):
-      cake_weight = cake[0]
-      cake_value = cake[1]
-      max_number_of_bags = capacity / cake_weight
-      return max_number_of_bags * cake_value
+        cake_weight = cake[0]
+        cake_value = cake[1]
+        max_number_of_bags = capacity / cake_weight
+        return max_number_of_bags * cake_value
+
+    def sort_cakes(self,cakes, capacity):
+	# need to work on the below for muscle memory
+	return sorted(cakes,key=lambda cake: self.get_max_cake_value(cake,capacity), reverse=True)
 
 class TestCakeThief(unittest.TestCase):
     def setUp(self):
@@ -23,4 +27,4 @@ class TestCakeThief(unittest.TestCase):
 
     def test_sort_cakes(self):
 	sorted_cakes = [(3,90), (7,160), (2,15)]
-	self.assertEquals(self.cake_thief.sort_cakes(self.cakes), sorted_cakes)
+	self.assertEquals(self.cake_thief.sort_cakes(self.cakes, self.capacity), sorted_cakes)
