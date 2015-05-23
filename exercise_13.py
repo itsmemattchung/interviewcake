1b5f6397ba95fc1c048f1a5b8b0427f77e331d80
matthewchung <itsmemattchung@gmail.com>
2015-05-22 23:25:20 -0700

    Functional but should be converted to binary search

--- exercise_13.py
+++ exercise_13.py
@@ -5,7 +5,19 @@ import unittest
 class TestRotationPoint(unittest.TestCase):
 
     def find_rotation_point(self, words):
-        pass
+	# aaabcd
+	# aaabbc
+	# find first letter between `a`, and `b` that is different
+	# the first letter that is different. if `b` is greater, than skip to next item
+	# if `b` at the index is less than, that is the rotation point
+	char_of_words = [ list(word) for word in words ]
+	for i, item in enumerate(char_of_words[:-1]):
+	        for letter_index, letter in enumerate(char_of_words[i]):
+			if letter < char_of_words[i+1][letter_index]:
+			    break
+	    		else:
+			    return i + 1
+            
 
 
     def setUp(self):
